06850f56da77a0aa21d9efdd903d975e5c69a6ac
Uvindu Bro <79355885+UvinduBro@users.noreply.github.com>
2021-04-23 14:41:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -53,9 +53,9 @@ def main():
                 Alty.read_history(bot)
 
             utc_now = datetime.datetime.now(pytz.timezone('UTC')).strftime("%I:%M %p %d/%m/%y")
-            ma_now = datetime.datetime.now(pytz.timezone('Africa/Casablanca')).strftime("%d/%m/%y %I:%M:%S %p")
+            ma_now = datetime.datetime.now(pytz.timezone('SriLanka/Colombo')).strftime("%d/%m/%y %I:%M:%S %p")
 
-            TEXT += f"\n⏱ <b>LAST UPDATE :</b>\n\n🌎 UTC : {str(utc_now)}\n🇲🇦 MA : {str(ma_now)}"
+            TEXT += f"\n⏱ <b>LAST UPDATE :</b>\n\n🌎 UTC : {str(utc_now)}\n🇲🇦 SL: {str(ma_now)}"
 
             Alty.edit_message_text(UPDATE_CHANNEL, STATUS_MESSAGE_ID, text=TEXT, disable_web_page_preview=True, parse_mode="html")
             print(f"[INFO] Everything Done! Sleeping For {round(TIME / 60)} Hours...")
